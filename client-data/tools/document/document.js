1440f66c849825af4da3450d28549d8e72203b8e
unknown <raghavgoel847@gmail.com>
2023-06-30 11:24:43 +0530

    Initial Commit

---
+++ client-data/tools/document/document.js
@@ -0,0 +1,78 @@
+﻿(function documents() { //Code isolation
+
+
+// This isn't an HTML5 canvas, it's an old svg hack, (the code is _that_ old!)
+var uploadSVG = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 17 17"><g id="Layer_2" data-name="Layer 2"><g id="Layer_1-2" data-name="Layer 1"><polygon points="16 9 16 16 1 16 1 9 0 9 0 17 17 17 17 9 16 9"/><polygon points="8.5 0 15.15 6.65 14.44 7.35 9 1.91 9 13 8 13 8 1.91 2.56 7.35 1.85 6.65 8.5 0"/></g></g></svg>';
+var xlinkNS = "http://www.w3.org/1999/xlink";
+var imgCount = 1;
+var fileInput;
+function onstart() {
+    fileInput = document.createElement("input");
+    fileInput.type = "file";
+    fileInput.accept = "image/*";
+    fileInput.click();
+    fileInput.addEventListener("change", function(){
+        var reader = new FileReader();
+        reader.readAsDataURL(fileInput.files[0]);
+      
+        reader.onload = function (e) {
+            var image = new Image();
+            image.src = e.target.result;
+            image.onload = function () {
+    
+            var uid = Tools.generateUID("doc"); // doc for document
+            // console.log(image.src.toString().length);
+            
+            var msg = {
+                id: uid,
+                type:"doc",
+                src: image.src,
+                w: this.width || 300,
+                h: this.height || 300,
+                x: (100+document.documentElement.scrollLeft)/Tools.scale+10*imgCount,
+                y: (100+document.documentElement.scrollTop)/Tools.scale + 10*imgCount
+                //fileType: fileInput.files[0].type
+            };
+            draw(msg);
+            Tools.send(msg,"Document");
+            imgCount++;
+            };
+        };
+       // Tools.change(Tools.prevToolName);
+    });
+}
+
+function draw(msg) {
+    //const file = self ? msg.data : new Blob([msg.data], { type: msg.fileType });
+    //const fileURL = URL.createObjectURL(file);
+
+   // fakeCanvas.style.background = `url("${fileURL}") 170px 0px no-repeat`;
+    //fakeCanvas.style.backgroundSize = "400px 500px";
+    var aspect = msg.w/msg.h
+    var img = Tools.createSVGElement("image");
+    img.id=msg.id;
+    img.setAttribute("class", "layer-"+Tools.layer);
+    img.setAttributeNS(xlinkNS, "href", msg.src);
+    img.x.baseVal.value = msg['x'];
+    img.y.baseVal.value = msg['y'];
+    img.setAttribute("width", 400*aspect);
+    img.setAttribute("height", 400);
+    if(msg.transform)
+			img.setAttribute("transform",msg.transform);
+    Tools.group.appendChild(img);
+    
+}
+
+Tools.add({
+    "name": "Document",
+    // "icon": "🖼️",
+    iconHTML: uploadSVG,
+    "shortcuts": {
+        "changeTool":"7"
+    },
+    "draw": draw,
+    "onstart": onstart,
+    "oneTouch":true
+});
+
+})(); //End of code isolation
---
+++ client-data/tools/hand/hand.js
@@ -0,0 +1,67 @@
+﻿/**
+ *                        WHITEBOPHIR
+ *********************************************************
+ * @licstart  The following is the entire license notice for the
+ *  JavaScript code in this page.
+ *
+ * Copyright (C) 2013  Ophir LOJKINE
+ *
+ *
+ * The JavaScript code in this page is free software: you can
+ * redistribute it and/or modify it under the terms of the GNU
+ * General Public License (GNU GPL) as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option)
+ * any later version.  The code is distributed WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU GPL for more details.
+ *
+ * As additional permission under GNU GPL version 3 section 7, you
+ * may distribute non-source (e.g., minimized or compacted) forms of
+ * that code without the copy of the GNU GPL normally required by
+ * section 4, provided you include this license notice and a URL
+ * through which recipients can access the Corresponding Source.
+ *
+ * @licend
+ */
+
+(function () { //Code isolation
+
+	var dragHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 17 17"><g id="Layer_2" data-name="Layer 2"><g id="Layer_1-2" data-name="Layer 1"><polygon points="16 15.29 10.35 9.65 9.65 10.35 15.29 16 11 16 11 17 17 17 17 11 16 11 16 15.29"/><polygon points="11 0 11 1 15.29 1 9.65 6.65 10.35 7.35 16 1.71 16 6 17 6 17 0 11 0"/><polygon points="6.65 9.65 1 15.29 1 11 0 11 0 17 6 17 6 16 1.71 16 7.35 10.35 6.65 9.65"/><polygon points="6 1 6 0 0 0 0 6 1 6 1 1.71 6.65 7.35 7.35 6.65 1.71 1 6 1"/></g></g></svg>';
+	var orig = { x: 0, y: 0 };
+	var pressed = false;
+	function press(x, y, evt, isTouchEvent) {
+		if (!isTouchEvent) {
+			pressed = true;
+			orig.x = scrollX + evt.clientX;
+			orig.y = scrollY + evt.clientY;
+		}
+	}
+	function move(x, y, evt, isTouchEvent) {
+		if (pressed && !isTouchEvent) { //Let the browser handle touch to scroll
+			window.scrollTo(orig.x - evt.clientX, orig.y - evt.clientY);
+		}
+	}
+	function release() {
+		pressed = false;
+	}
+
+	Tools.add({ //The new tool
+		// "name": "Hand",
+	// "icon": "✋",
+	"iconHTML": dragHTML,
+        "name": "Hand",
+		//"icon": "",
+		"shortcuts": {
+            "changeTool":"8"
+        },
+		"listeners": {
+			"press": press,
+			"move": move,
+			"release": release
+		},
+		"mouseCursor": "move"
+	});
+
+	//The hand tool is selected by default
+	Tools.change("Hand");
+})(); //End of code isolation
